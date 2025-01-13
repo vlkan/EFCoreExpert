1df9d422e972dd861b36166156f622079249d8f2
vlkan <thkvolkanonder@gmail.com>
2025-01-13 17:18:46 +0300

    dbcontext configuration

---
+++ EFDemo/Infra/Context/MovieDbContext.cs
@@ -0,0 +1,46 @@
+﻿using EFDemo.Infra.Entities;
+using EFDemo.Infra.EntityTypeConfigurations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EFDemo.Infra.Context;
+
+public class MovieDbContext : DbContext
+{
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Director> Directors { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+    public DbSet<Actor> Actors { get; set; }
+
+    public MovieDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public MovieDbContext()
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("ef");
+
+        //IEntityTypeConfiguration kullanan configleri otomatik ekle
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MovieEntityConfiguration).Assembly);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (optionsBuilder.IsConfigured)
+            return;
+
+        var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        var connStr = configuration.GetConnectionString("SqlServer");
+
+        optionsBuilder.UseSqlServer(connStr, options =>
+        {
+            options.CommandTimeout(5_000);
+            options.EnableRetryOnFailure(maxRetryCount: 5);
+        });
+    }
+
+}
--- EFDemo/Program.cs
+++ EFDemo/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EFDemo.Infra.Context;
+using Microsoft.EntityFrameworkCore;
+
+var dbContext = new MovieDbContext();
+
+dbContext.Database.OpenConnection();
+
+Console.ReadLine();
