e22d8f124f1fee04a341f0e189394f64e01ae103
vlkan <thkvolkanonder@gmail.com>
2025-01-13 15:46:36 +0300

    data annotation inceleme

--- EFDemo/Infra/Entities/Base/BaseEntity.cs
+++ EFDemo/Infra/Entities/Base/BaseEntity.cs
@@ -8,6 +8,7 @@ namespace EFDemo.Infra.Entities.Base;
 
 public class BaseEntity
 {
+    //[Key]
     public Guid Id { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? ModifiedAt { get; set; }
